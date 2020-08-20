92c88c15de349cf4110ff9281e5293fe1ce83fff
nitika <guptanitika14@gmail.com>
2020-08-20 09:05:28 -0400

    first commit

---
+++ src/test/java/NitikaGupta/Mavenjava/SeleniumTest.java
@@ -0,0 +1,24 @@
+package NitikaGupta.Mavenjava;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from Maven
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("BrowserAutomation");
+	}
+	
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+	
+
+}
